4002e04944d555fb6c569bbf440f0f463fb14a28
IkuSenpai <dmorley1.dm@gmail.com>
2026-06-06 15:48:29 -0700

    Update help command list

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -21,13 +21,41 @@ module.exports = {
           ].join('\n'),
         },
         {
-          name: 'Coming Soon',
+          name: 'Schedules',
           value: [
-            '`/online` - Shows online player count.',
-            '`/guilds` - Shows guild information.',
-            '`/fishkings` - Shows Fish King schedule.',
-            '`/worldbosses` - Shows World Boss schedule.',
-            '`/eidolons` - Shows Eidolon information.',
+            '`/fishkings` - Shows Fish King schedule information.',
+            '`/worldbosses` - Shows World Boss schedule information.',
+            '`/guildbosses` - Shows Guild Boss schedule information.',
+            '`/cardrangers` - Shows Card Ranger schedule information.',
+            '`/quiz` - Shows Aura Kingdom Quiz schedule information.',
+            '`/raids` - Shows raid schedule information.',
+            '`/resets` - Shows dungeon reset schedule information.',
+            '`/battlefields` - Shows Battlefield schedule information.',
+            '`/archaeology` - Shows Archaeology site information.',
+          ].join('\n'),
+        },
+        {
+          name: 'Server',
+          value: [
+            '`/paragon` - Shows the currently active Paragon.',
+            '`/stats` - Shows stats from Eidolon prayers.',
+          ].join('\n'),
+        },
+        {
+          name: 'Eidolons',
+          value: [
+            '`/eidolonspawns` - Shows Eidolon instance spawn information.',
+            '`/eidolons` - Shows all available Eidolons.',
+            '`/eidolon` - Shows specific Eidolon information.',
+            '`/prayers` - Shows Eidolon prayer information.',
+            '`/prayer` - Shows specific Eidolon prayer information.',
+          ].join('\n'),
+        },
+        {
+          name: 'Database Commands Coming Later',
+          value: [
+            '`/online` - Shows online players.',
+            '`/guilds` - Shows in-game guilds.',
           ].join('\n'),
         }
       )
